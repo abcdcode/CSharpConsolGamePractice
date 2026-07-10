0e9fc94ad2921c494bcd9006773a8f3a1f4e084d
abcdcode <kj022220@gmail.com>
2026-07-10 10:13:51 +0900

    pdf

--- ConsoleShootingGame/obj/Debug/net10.0/ConsoleShootingGame.AssemblyInfo.cs
+++ ConsoleShootingGame/obj/Debug/net10.0/ConsoleShootingGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleShootingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632d33fa2099e63b2bd76be5310e09f59812c75d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1b8a1ae6d12534bc684bec6140c3a6241f0f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleShootingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleShootingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
